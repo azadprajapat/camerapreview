2e26a54e6297c6dcab93f2b6839c4e372c3fa6d3
azad prajapat <azadprajapat4@gmail.com>
2020-06-23 03:49:21 +0530

    set the capture points over the sphere

--- lib/camerapreview.dart
+++ lib/camerapreview.dart
@@ -28,39 +28,67 @@ class _HomeState extends State<Home> {
   Future<void> _initialiseControllerFuture;
   var pitch;
   var roll;
-  int number=0;
-  List<Image_set> _imglist=[];
-  var A;
-  var E=0;
+  List _list1 = [
+    {"A": "100", "E": "20"},
+    {"A": "140", "E": "20"},
+    {"A": "180", "E": "20"},
+    {"A": "220", "E": "20"},
+    {"A": "260", "E": "20"},
+    {"A": "100", "E": "48"},
+    {"A": "140", "E": "48"},
+    {"A": "180", "E": "48"},
+    {"A": "220", "E": "48"},
+    {"A": "260", "E": "48"},
+    {"A": "100", "E": "76"},
+    {"A": "140", "E": "76"},
+    {"A": "180", "E": "76"},
+    {"A": "220", "E": "76"},
+    {"A": "260", "E": "76"},
+  ];
+  List<Points> _list = List<Points>();
+  List<Points> _visible_point_list = List<Points>();
+  int list_index;
+  int number = 0;
+  List<Image_set> _imglist = [];
   var azimuth;
-   StreamSubscription<dynamic> _streamSubscriptions;
-   StreamSubscription<dynamic> _streamSubscriptions2;
-   @override
+  StreamSubscription<dynamic> _streamSubscriptions;
+  StreamSubscription<dynamic> _streamSubscriptions2;
+
+  @override
   void initState() {
+    _list1.forEach((element) {
+      _list.add(Points(A: element['A'], E: element['E']));
+    });
+    _visible_point_list.add(_list[0]);
+      print("# STATUS 200 VISIBLE LIST");
     super.initState();
     _streamSubscriptions = AeyriumSensor.sensorEvents.listen((event) {
       setState(() {
-        pitch = ((event.pitch)*180/math.pi).toInt();
-        roll = (event.roll).toInt() ;
+        pitch = ((event.pitch) * 180 / math.pi).toInt();
+        roll = (event.roll).toInt();
       });
     });
-    _streamSubscriptions2=FlutterCompass.events.listen((event) {
+    _streamSubscriptions2 = FlutterCompass.events.listen((event) {
       setState(() {
-        azimuth=event.toInt();
-      if(number==0) {
-        A = event.toInt();
-      }
+        azimuth = event.toInt();
       });
     });
-    _controller = camera_.CameraController(widget.camera, camera_.ResolutionPreset.ultraHigh);
+    _controller = camera_.CameraController(
+        widget.camera, camera_.ResolutionPreset.medium);
     _initialiseControllerFuture = _controller.initialize();
-     Timer.periodic(Duration(seconds: 1), (timer) {
-      if(A-azimuth<=1&&A-azimuth>=-1&&E-pitch>=-1&&E-pitch<=1){
-        TakePhoto();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+    _visible_point_list.forEach((element) {
+      var current_A=int.parse(element.A);
+      var current_E=int.parse(element.E);
+      if(current_A-azimuth<=1&&current_A-azimuth>=-1&&current_E-pitch>=-1&&current_E-pitch<=1){
+        TakePhoto(_visible_point_list.indexOf(element),current_A,current_E);
+        print("# STATUS 200 PHOTO CAPTURED");
       }
+    });
 
     });
   }
+
   @override
   void dispose() {
     if (_streamSubscriptions != null) {
@@ -72,56 +100,100 @@ class _HomeState extends State<Home> {
     _controller.dispose();
     super.dispose();
   }
-  Widget build(BuildContext context ) {
+
+  Widget build(BuildContext context) {
     return Scaffold(
-         body: Stack(
-
-          children: <Widget>[
-            CustomPaint(
-              foregroundPainter: MyPainter(
-                  pitch: pitch, screen: MediaQuery.of(context).size,A:A,E: E,azimuth: azimuth,roll: roll,n: number,ImgList: _imglist),
-                      child: FutureBuilder<void>(
-                        future: _initialiseControllerFuture,
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.done) {
-                            return  Container(
-                              child: camera_.CameraPreview(_controller),
-                            );
-                          } else {
-                            return Center(child: CircularProgressIndicator());
-                          }
-                        },
-                      ),
-
-            ),
-           ],
-        ));
+        body: Stack(
+      children: <Widget>[
+        CustomPaint(
+          foregroundPainter: MyPainter(
+              pitch: pitch,
+              screen: MediaQuery.of(context).size,
+              azimuth: azimuth,
+              roll: roll,
+              n: number,
+              ImgList: _imglist,
+              list: _list,
+              visible_list: _visible_point_list),
+          child: FutureBuilder<void>(
+            future: _initialiseControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Container(
+                  child: camera_.CameraPreview(_controller),
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ),
+      ],
+    ));
   }
-    void TakePhoto() async{
-    await _initialiseControllerFuture;
-    final path = join(
-        (await getTemporaryDirectory()).path,
-        '${DateTime.now()}.png');
 
-    await _controller.takePicture(path);
-   Image_set _set =Image_set(azimuth: A,elevation: pitch,Imagepath: path);
-   _imglist.add(_set);
-
-    print("# STATUS 200 IMAGE CAPTURED");
-    setState(() {
-      number++;
+  void TakePhoto(int index,int current_A,int current_E) async {
+ //   await _initialiseControllerFuture;
+  //  final path =
+    //    join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
+  //  await _controller.takePicture(path);
+    var path="# STATUS 200 PATH";
+    print("#1 STATUS 200 IMAGE CAPTURED");
+   setState(() {
+          Image_set _set =Image_set(azimuth:current_A,elevation:current_E,Imagepath: path);
+         _imglist.add(_set);
+           number++;
+          _visible_point_list.removeAt(index);
     });
+    print("#2 STATUS 200 ${_visible_point_list.length} ");
+
+    _list.forEach((element) {
+     var _listA=int.parse(element.A);
+     var _listE=int.parse(element.E);
+     if(_listA-current_A==0&&_listE-current_E==0){
+       print("#3 STATUS 200");
+       setState(() {
+         list_index=_list.indexOf(element);
+       });
+       print("#4 STATUS 200");
+       return null;
+     }
+   });
     setState(() {
-      E=E;
-      A=A+30;
+      _list.removeAt(list_index);
+    });
+    print("#5 STATUS 200 ${_list.length}");
+    _list.forEach((element) {
+      var _listA=int.parse(element.A);
+      var _listE=int.parse(element.E);
+      if((_listA-current_A<50&&(_listA-current_A>0)&&(_listE-current_E)==0)
+          ||(_listA-current_A>-50&&(_listA-current_A<0)&&(_listE-current_E)==0)
+          ||(_listA-current_A==0&&(_listE-current_E)<40&&(_listE-current_E)>0)
+          ||(_listA-current_A==0&&(_listE-current_E)>-40&&(_listE-current_E)<0)){
+        print("#6 STATUS 200");
+        setState(() {
+        _visible_point_list.add(element);
+      });
+      }
 
     });
-   }
+    print("#7 STATUS 200 ${_visible_point_list.length} & ${_list.length}");
+    _visible_point_list.forEach((element) {
+      print("${element.A} & ${element.E}");
+    });
+  }
 }
+
 class Image_set {
   final Imagepath;
   final azimuth;
   final elevation;
-  Image_set({this.elevation,this.azimuth,this.Imagepath});
+  Image_set({this.elevation, this.azimuth, this.Imagepath});
+}
+
+class Points {
+  final A;
+  final E;
+
+  Points({this.A, this.E});
 }
--- lib/painter.dart
+++ lib/painter.dart
@@ -14,11 +14,11 @@ class MyPainter extends CustomPainter{
   final roll;
   final ImgList;
   final azimuth;
-  final A;
   final n;
-  final E;
+  final list;
+  final visible_list;
 
-  MyPainter({this.pitch,this.screen,this.A,this.E,this.azimuth,this.roll,this.ImgList,this.n});
+  MyPainter({this.pitch,this.screen,this.azimuth,this.roll,this.ImgList,this.n,this.list,this.visible_list});
   @override
 
 
@@ -36,12 +36,10 @@ class MyPainter extends CustomPainter{
     Paint Captured= new Paint()
     ..color=Colors.red
     ..style=PaintingStyle.fill;
-
     var x_center=screen.width/2;
     var y_center=screen.height/2;
-
-    var c_width=200;
-    var c_heigth=300;
+    var c_width=250;
+    var c_heigth=400;
 
 //To draw mini directional triangle
     var path1= Path();
@@ -54,24 +52,17 @@ class MyPainter extends CustomPainter{
     path2.lineTo(x_center-15, y_center+50);
     path2.lineTo(x_center+15, y_center+50);
     path2.close();
-
     var path3= Path();
     path3.moveTo(x_center-65, y_center);
     path3.lineTo(x_center-50, y_center-15);
     path3.lineTo(x_center-50, y_center+15);
     path3.close();
-
     var path4= Path();
     path4.moveTo(x_center+65, y_center);
     path4.lineTo(x_center+50, y_center-15);
     path4.lineTo(x_center+50, y_center+15);
     path4.close();
 
-    var start_P1_x = ((A-azimuth)*screen.width/66.19);
-    var start_P1_y = ((pitch-E)*screen.height/52.09);
-
-    var   x =  ((start_P1_x*cos(roll)-start_P1_y*sin(-roll))+screen.width/2);
-    var  y =  ((start_P1_x*sin(-roll)+start_P1_y*cos(roll))+screen.height/2);
 
     //drawing boundary over image
     canvas.drawRect(Rect.fromPoints(Offset(0,0), Offset(screen.width,(screen.height-c_heigth)/2)), paint);
@@ -82,52 +73,51 @@ class MyPainter extends CustomPainter{
 
     //working with image display on the screen
     if(n!=0){
-      if(ImgList!=null){
-        for(int i=0;i<n;i++) {
-          var A1=ImgList[i].azimuth-18.5;
-         var A2=ImgList[i].azimuth+18.5;
-          var cx1 = ((A1- azimuth) * screen.width / 66.19) +
-              screen.width / 2;
-          var cy1 = ((pitch - ImgList[i].elevation+11.5) * screen.height / 52.09) +
-              screen.height / 2;
-          var cx2 = ((A2 - azimuth) * screen.width / 66.19) +
-              screen.width / 2;
-          var cy2 = ((pitch - ImgList[i].elevation-11.5) * screen.height / 52.09) +
-              screen.height / 2;
-            canvas.drawRect(Rect.fromPoints(
-              Offset(cx1, cy1), Offset(cx2, cy2)),
-              Captured);
+//         for(int i=0;i<n;i++) {
+//          var e1=ImgList[i].azimuth-18.5;
+//          var e2=ImgList[i].azimuth+18.5;
+//            var cx1 = ((e1- azimuth) * screen.width / 66.19) +
+//                screen.width / 2;
+//            var cy1 = ((pitch - ImgList[i].elevation+11.5) * screen.height / 52.09) +
+//                screen.height / 2;
+//            var cx2 = ((e2 - azimuth) * screen.width / 66.19) +
+//                screen.width / 2;
+//            var cy2 = ((pitch - ImgList[i].elevation-11.5) * screen.height / 52.09) +
+//                screen.height / 2;
+//            canvas.drawRect(Rect.fromPoints(
+//                Offset(cx1, cy1), Offset(cx2, cy2)),
+//                Captured);
+//       }
+     }
+    visible_list.forEach((element){
+      var start_P1_x;
+      var start_P1_y;
+      var x;
+      var y;
+      start_P1_x = ((int.parse(element.A)-azimuth)*screen.width/66.19);
+      start_P1_y = ((pitch-int.parse(element.E))*screen.height/52.09);
+      x =  ((start_P1_x*cos(roll)-start_P1_y*sin(-roll))+screen.width/2);
+      y =  ((start_P1_x*sin(-roll)+start_P1_y*cos(roll))+screen.height/2);
+      canvas.drawCircle(Offset(x,y), 20.0, circle);
+      if(n==0){
+        if(y_center-y >10){
+          canvas.drawPath(path1, Center_circle);
+        }
+        else if(y_center-y<10&&y_center-y>-10){
+        }else{
+          canvas.drawPath(path2, Center_circle);
+        }
+        if(x_center-x >10){
+          canvas.drawPath(path3, Center_circle);
+        }
+        else if(x_center-x<10&&x_center-x>-10){
+        }else{
+          canvas.drawPath(path4, Center_circle);
         }
       }
-     }
-
-
-
-
-    if(y_center-y >10){
-      canvas.drawPath(path1, Center_circle);
-
-    }
-    else if(y_center-y<10&&y_center-y>-10){
-    }else{
-      canvas.drawPath(path2, Center_circle);
-    }
-
-    if(x_center-x>10){
-      canvas.drawPath(path3, Center_circle);
-    }else if(x_center-x<10&&x_center-x>-10){
-    }
-    else{
-      canvas.drawPath(path4, Center_circle);
-    }
-
-    canvas.drawRect(Rect.fromPoints(Offset((screen.width-c_width)/2,(screen.height-c_heigth)/2), Offset((screen.width+c_width)/2,(screen.height+c_heigth)/2)), Center_circle);
+    });
+     canvas.drawRect(Rect.fromPoints(Offset((screen.width-c_width)/2,(screen.height-c_heigth)/2), Offset((screen.width+c_width)/2,(screen.height+c_heigth)/2)), Center_circle);
     canvas.drawCircle(Offset(x_center,y_center), 30, Center_circle);
-    canvas.drawCircle(Offset(x,y), 20.0, circle);
-
-
-
-
   }
 
   @override
